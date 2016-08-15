f381c48f2122a704a89e1b5c541b0261c6647201
Michaël Zasso <targos@protonmail.com>
2016-08-15 17:05:16 +0200

    flex layout

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,18 @@ import {connect} from 'react-redux';
 class App extends React.Component {
     render() {
         return (
-            <div>
-                Hello {this.props.name}!
+            <div style={{position: 'relative', height: '100%', width: '100%', display: 'flex', flexDirection: 'column'}}>
+                <div style={{height: '40px'}}>
+                    top menu
+                </div>
+                <div style={{flex: 1, display: 'flex'}}>
+                    <div style={{width: '200px', display: 'flex', flexDirection: 'column'}}>
+                        table
+                    </div>
+                    <div style={{flex: 1}}>
+                        Hello {this.props.name}!
+                    </div>
+                </div>
             </div>
         );
     }
